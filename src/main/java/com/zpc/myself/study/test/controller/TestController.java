acb4ca772184ff5c677cae4a74bccfc49139e13c
zpc <1573421090@qq.com>
2021-11-28 23:17:19 +0800

    渲染jsp页面

--- src/main/java/com/zpc/myself/study/test/controller/TestController.java
+++ src/main/java/com/zpc/myself/study/test/controller/TestController.java
@@ -1,14 +1,35 @@
 package com.zpc.myself.study.test.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
-@RestController
+//@RestController
+@Controller
 public class TestController {
 
-    @PostMapping("/hello/controller")
+    @GetMapping("/hello/controller")
     public Object test() {
         return "hello myself test";
     }
+
+    @GetMapping("/html")
+    public Object test2() {
+        return "testOne";
+    }
+
+    @GetMapping("/jsp1")
+    public ModelAndView jspTest() {
+        return new ModelAndView("jspTest");
+    }
+
+    @GetMapping("/jsp2")
+    public Object jspTest2() {
+        return "jspTest";
+    }
+
+
 }
